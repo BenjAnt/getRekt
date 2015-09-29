c83aa187938566705669a0b7c5c739c12febe145
Benjamin <vidili.benjamin@bbox.fr>
2015-09-29 18:12:34 +0200

    Affichage du sprite + mouvement vers le haut lors de la pression de la touche espace

--- main.c
+++ main.c
@@ -24,10 +24,11 @@ void pause(void)
 
 int main(void)
 {
- SDL_Surface *ecran = NULL, *background = NULL;
-  SDL_Rect backgroundCoord;
+ SDL_Surface *ecran = NULL, *background = NULL,*superman =NULL;
+  SDL_Rect backgroundCoord,supermanCoord;
   int i,j;
-  
+  int continuer =1;
+SDL_Event event;
   object_list * L = NULL;
   object o;
   L = readPipe(L);
@@ -49,12 +50,71 @@ int main(void)
   if(!background) printf("gg");
   SDL_BlitSurface(background, NULL, ecran, &backgroundCoord);
   SDL_Flip(ecran);
+  
+  superman=SDL_LoadBMP("superman.bmp");
+SDL_SetColorKey(superman, SDL_SRCCOLORKEY, SDL_MapRGB(superman->format,0 ,255 ,0));
+  supermanCoord.x = ecran->w / 2 - superman->w / 2;
+  supermanCoord.y = ecran->h / 2 - superman->h / 2;
+
+  while (continuer)
+
+    {
+
+        SDL_WaitEvent(&event);
+
+        switch(event.type)
+
+        {
+
+            case SDL_QUIT:
+
+                continuer = 0;
+
+                break;
+
+            case SDL_KEYDOWN:
+
+                switch(event.key.keysym.sym)
+
+                {
+		  
+		case SDLK_SPACE: // barre espace
+
+                        supermanCoord.y -= 20;
+
+                        break;
+		  
+             
+		  
+                }
+  
+                
+	}
+        
+ 
+
+     
+
+  SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+        /* On place Zozor à sa nouvelle position */
+
+        SDL_BlitSurface(superman, NULL, ecran, &supermanCoord);
+
+        /* On met à jour l'affichage */
+
+        SDL_Flip(ecran);
+
+	}
+    
+
+
   pause(); // Mise en pause du programme
   
 
   //main_loop(L);
   SDL_FreeSurface(background);
-  
+  SDL_FreeSurface(superman);
   SDL_Quit(); // Arrêt de la SDL
   return 0;
+    
 }
