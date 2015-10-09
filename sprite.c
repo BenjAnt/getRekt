cda58304b09575cd36fad8a98542996489592729
Benjamin <vidili.benjamin@bbox.fr>
2015-10-09 16:34:41 +0200

    Modification mouvement du sprite.

--- sprite.c
+++ sprite.c
@@ -7,12 +7,15 @@ SDL_Rect backgroundCoord;
 SDL_Rect supermanCoord ;
 int continuer = 1;
 SDL_Event event;
+int gameover = 0 ;
+
 void draw_sprite(object sprite){
   superman=SDL_LoadBMP("superman.bmp");
   supermanCoord.x=10;
   supermanCoord.y=100;
   SDL_SetColorKey(superman, SDL_SRCCOLORKEY, SDL_MapRGB(superman->format,0 ,255 ,0));
   SDL_BlitSurface(superman, NULL, ecran, &supermanCoord);
+  
 }
 
 void draw_background(object sprite){ 
@@ -24,7 +27,8 @@ void draw_background(object sprite){
 }
 void bouger_sprite() {
   while (continuer){
-    SDL_WaitEvent(&event);
+    //SDL_WaitEvent(&event);
+SDL_PollEvent(&event);
     switch(event.type){
       case SDL_QUIT:
 	continuer = 0;
@@ -35,16 +39,26 @@ void bouger_sprite() {
 	       event.key.keysym.unicode);
 	switch(event.key.keysym.sym) {		  
 	case SDLK_SPACE: // barre espace	  
-	  supermanCoord.y -= 3;
+	  supermanCoord.y -= 5;
+        
 	  //printf("aa");
 	  break;}
     case SDL_KEYUP:
-      printf("Touche %d relâchée\n", event.key.keysym.sym);
+      supermanCoord.y += 1;
+      while(supermanCoord.y>400 || supermanCoord.y < 0){
+	continuer =0;
+	break;}
+      
       break;
+   
     }
-    //SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-    SDL_BlitSurface(superman, NULL, ecran, &supermanCoord);
-    SDL_Flip(ecran);
+
+    SDL_BlitSurface(background, NULL, ecran, &backgroundCoord);
+SDL_BlitSurface(superman, NULL, ecran, &supermanCoord);
+
+SDL_Flip(ecran);
+  
+   
   }
   
   
